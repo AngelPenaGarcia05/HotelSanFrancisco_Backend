ed3504cec8887abee777abf25c4762d5323fecdf
CarlosHuamanM <thecarlox2000@gmail.com>
2026-06-18 00:53:02 -0500

    feat(reportes): agregar interfaz ReportService para generación de reportes

---
+++ src/main/java/com/sanfrancisco/api/modules/reportes/service/interfaces/ReportService.java
@@ -0,0 +1,18 @@
+package com.sanfrancisco.api.modules.reportes.service.interfaces;
+
+import com.sanfrancisco.api.modules.reportes.dto.request.ReportRangeRequest;
+import com.sanfrancisco.api.modules.reportes.dto.response.ManagementDashboardResponse;
+import com.sanfrancisco.api.modules.reportes.dto.response.OccupancyReportResponse;
+import com.sanfrancisco.api.modules.reportes.dto.response.ReservationsReportResponse;
+import com.sanfrancisco.api.modules.reportes.dto.response.RevenueReportResponse;
+
+public interface ReportService {
+
+    RevenueReportResponse buildRevenueReport(ReportRangeRequest range);
+
+    ReservationsReportResponse buildReservationsReport(ReportRangeRequest range);
+
+    OccupancyReportResponse buildOccupancyReport(ReportRangeRequest range);
+
+    ManagementDashboardResponse buildManagementDashboard(ReportRangeRequest range);
+}
